cdc7551ab800a972085eef88f869ec0226fbb358
Gérald Tibi <gerald_tibi@natural-solutions.eu>
2016-08-02 10:59:36 +0200

    Ajout première version Centralisation

---
+++ Centralisation/InsertConfTableACopier.sql
@@ -0,0 +1,116 @@
+
+
+DELETE FROM [SourceTarget_Table]
+DELETE FROM [TableACopier]
+DELETE FROM [SourceTarget]
+
+INSERT INTO [dbo].[SourceTarget]
+           ([SourceDatabase]
+           ,[TargetDatabase])
+		   VALUES ('NARC_TRACK_MACQ_New.dbo.','NARC_TRACK_MACQ.dbo.')
+
+
+
+INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TProtocole'
+           ,'TPro_pk_id'
+           ,5)
+
+
+INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TObservation'
+           ,'TObs_pk_id'
+           ,10)
+
+INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TType'
+           ,'TTyp_pk_id'
+           ,1)
+
+		INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TTTypeBase'
+           ,'TTBse_pk_id'
+           ,0)
+		
+		INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('Tunite'
+           ,'TUni_pk_id'
+           ,0)
+
+
+		INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TTProtocole'
+           ,'TTpro_PK_ID'
+           ,0)
+
+		INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TTFrequence'
+           ,'TTFre_PK_ID'
+           ,0)
+
+		   INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TChampLie'
+           ,'TCLie_PK_ID'
+           ,0)
+		   
+
+
+		   INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TActivite'
+           ,'TAct_PK_ID'
+           ,0)
+
+INSERT INTO [dbo].[TableACopier]
+           ([Name]
+           ,[IdNamere]
+           ,[OrdreExecution])
+     VALUES
+           ('TAsyncProcessList'
+           ,'TAPL_PK_ID'
+           ,0)
+
+
+ INSERT INTO [dbo].[SourceTarget_Table]
+           ([fk_SourceTarget]
+           ,[fk_TableACopier])
+		   SELECT s.ID,t.ID FROM TableACopier T JOIN SourceTarget S ON s.SourceDatabase='NARC_TRACK_MACQ_New.dbo.'
+		   WHErE t.name in ('TProtocole','TObservation','TType','TTTypeBase','TTProtocole','TTFrequence','TChampLie','TActivite','TAsyncProcessList')
+
+		   
+ 		   
---
+++ Centralisation/TableACopier.sql
@@ -0,0 +1,32 @@
+
+-- TODO: Rajouter les foreign Key + primary Key
+
+
+CREATE TABLE TableACopier(
+[ID] [int] IDENTITY(1,1) 
+,[Name] [varchar](250) NOT NULL
+,[IdNamere] [varchar](250) NOT NULL
+,[OrdreExecution] INT NOT NULL
+)
+
+
+
+
+CREATE TABLE SourceTarget(
+[ID] [int] IDENTITY(1,1) 
+,[SourceDatabase] [varchar](250) NOT NULL
+,[TargetDatabase] [varchar](250) NOT NULL
+)
+
+
+
+
+
+
+CREATE TABLE SourceTarget_Table(
+fk_SourceTarget INT NULL
+,fk_TableACopier INT NULL
+)
+
+
+
